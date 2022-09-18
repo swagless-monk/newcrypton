4b8721f65491812c44bed0ba6fd2e2fc0860cc19
swagless-monk <72817022+swagless-monk@users.noreply.github.com>
2022-09-18 10:45:33 -0500

    Create backgroundButton.js

---
+++ static/js/backgroundButton.js
@@ -0,0 +1,13 @@
+// Tooltip Text 
+walletText = document.querySelector('#walletText');
+ursaText = document.querySelector('#ursaText');
+whaleText = document.querySelector('#whaleText');
+btcText = document.querySelector('#btcText');
+cryptoText = document.querySelector('#cryptoText');
+ethText = document.querySelector('#ethText');
+walletText.textContent = 'learn more about the performance of your wallet: transaction history, trends & future performance.';
+ursaText.textContent = 'learn about the current sentiment of the cryptocurrency market from twitter & reddit.';
+whaleText.textContent = 'learn how the whales are buying & selling & what they hold.';
+btcText.textContent = 'learn about bitcoin from its early beginning to the predictions of the future.';
+cryptoText.textContent = 'learn about current events in the world of cryptocurrency,';
+ethText.textContent = 'learn about the king of the alts and how one man opened the flood gates for the masses.';
